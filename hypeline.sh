160fc1218c268c80beff25339cf31b56c5ac08db
Kelly Sovacool <kellysovacool@gmail.com>
2018-02-19 18:48:05 -0500

    back to using bowtie2 and unpaired fastas

--- hypeline.sh
+++ hypeline.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# hypeline: a pipeline for haplotype calling on paired-end NGS data using bwa, freebayes, & whatshap
+# hypeline: a pipeline for haplotype calling on paired-end NGS data using bowtie2, freebayes, whatshap, & snp-sites
 # Author: Kelly Sovacool
 # Email: kellysovacool@uky.edu
 # 30 Nov. 2017
@@ -19,11 +19,14 @@ if [ ""${output_dir:$(expr ${#string} - 1):1} != "/" ]; then
 fi
 reference_name=$1  # do not include .fna extension
 reference_file=${reference_name}.fna
-if [ ! -e ${reference_name}.bwt ]; then
-	bwa index $reference_file
-fi
-if [ ! -e ${reference_file}.fai ]; then
+#if [ ! -e ${reference_name}.bwt ]; then
+#	bwa index $reference_file
+#fi
+if [ ! -e ${reference_name}.fai ]; then
 	samtools faidx $reference_file
+fi
+if [ ! -e ${reference_name}.1.bt2 ]; then
+	bowtie2-build $reference_file $reference_name
 fi
 inter_dir=intermediates/
 indiv_haps_dir=${inter_dir}individual_haps/
@@ -35,24 +38,31 @@ for dir in ${directories[@]}; do
 		echo "made dir $dir"
 	fi
 done
-for id in $( ls $input_dir | sed s/[-_].*$// | tr ' ' '\n' | sort -u | tr '\n' ' ' ); do
-	filename_r1="${input_dir}${id}_R1.fna"
-	filename_r2="${input_dir}${id}_R2.fna"
+#for id in $( ls $input_dir | sed s/\.*$// | tr ' ' '\n' | sort -u | tr '\n' ' ' ); do
+for infile in $( ls $input_dir ); do
+	id=$( echo $infile | sed s/\.*$// )
+	echo "Calling haplotypes for id ${id}"
 	sorted_bam=${inter_dir}${id}.sorted.bam	
-	if [ -f $filename_r1 ] && [ -f $filename_r2 ]; then 
-		bwa mem $reference_file $filename_r1 $filename_r2 | samtools view -S -b | samtools sort - -o $sorted_bam
-	else
-		if [ -f $filename_r1 ]; then
-			filename=$filename_r1
-		elif [ -f $filename_r2 ]; then
-			filename=$filename_r2
-		else
-			echo "neither $filename_r1 nor $filename_r2 exist"
-			break
-		fi
-		bwa mem $reference_file $filename | samtools view -S -b | samtools sort - -o $sorted_bam
-	fi
-
+	#filename_r1="${input_dir}${id}_R1.fq"
+	#filename_r2="${input_dir}${id}_R2.fq"
+	#if [ -f $filename_r1 ] && [ -f $filename_r2 ]; then 
+	#	#bwa mem $reference_file $filename_r1 $filename_r2 | samtools view -S -b | samtools sort - -o $sorted_bam
+	#	bowtie2 -x $reference_name -1 $filename_r1 -2 $filename_r2 | samtools view -S -b | samtools sort - -o $sorted_bam
+	#else
+	#	if [ -f $filename_r1 ]; then
+	#		filename=$filename_r1
+	#	elif [ -f $filename_r2 ]; then
+	#		filename=$filename_r2
+	#	else
+	#		echo "neither $filename_r1 nor $filename_r2 exist"
+	#		break
+	#	fi
+	#	#bwa mem $reference_file $filename | samtools view -S -b | samtools sort - -o $sorted_bam
+	#	bowtie2 -x $reference_name -U $filename | samtools view -S -b | samtools sort - -o $sorted_bam
+	#fi
+	filename=${input_dir}${infile}
+	echo "running bowtie on $filename"
+	bowtie2 -f -x $reference_name -U $filename | samtools view -S -b | samtools sort - -o $sorted_bam
 	filtered_vcf=${inter_dir}${id}.filtered.vcf
 	freebayes --min-base-quality 3 --min-mapping-quality 1 -f $reference_file $sorted_bam | bcftools filter -e 'QUAL < 20' | bcftools filter -e 'DP < 5' -o $filtered_vcf
 
@@ -69,6 +79,7 @@ for id in $( ls $input_dir | sed s/[-_].*$// | tr ' ' '\n' | sort -u | tr '\n' '
 	bcftools consensus -H 2 -f $reference_file $zipped_vcf > $hap2
 done
 
+echo "Combining haplotypes to single locus data"
 haps_to_single_loci.py $indiv_haps_dir $combined_haps_dir $reference_file
 
 for filename in $(ls $combined_haps_dir); do
